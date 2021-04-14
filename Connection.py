f5cbef109e6946a7e603cd574226f6014663e7af
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2021-04-13 22:37:00 -0500

    update connection

--- Connection.py
+++ Connection.py
@@ -1,4 +1,4 @@
-import socket
+import socket 
 import filetype
 class StablishConnection():
     
@@ -8,7 +8,6 @@ class StablishConnection():
         self.FORMAT = "utf-8"
         self.SIZE = 1024
         self.client = None
-        self.protocol = 0
         self.proto = protocol
         self.ADDR = (bytearray(),port)
         
@@ -29,11 +28,13 @@ class StablishConnection():
     
     def send_file(self, file_name):
         BASE_PATH = "Data/"
-        file_type = filetype.guess(f'{BASE_PATH}{file_name}')
         file_action='r'
+        
+        file_type = file_name.split(".")
 
+        print(file_type)
         #TODO: Agregar los file_new actions segun el tipo de archivo que se esta recibiendo
-        if file_type == "image" or file_type == "video" or file_type == "audio":
+        if file_type[1] == "jpg":
             file_action = "rb"
 
             while True:
@@ -76,32 +77,63 @@ class StablishConnection():
         #TODO: make the service background
         if self.proto == "tcp":
             self.protocol = socket.SOCK_STREAM 
+            
+            print("[EMPEZANDO] El servidor esta iniciando")
+            server = socket.socket(socket.AF_INET, self.protocol) # Se crea el servidor
+            server.bind(self.ADDR)  # Se establece la conexion
+            server.listen()  # Hacemos que el server este escuchando alguna peticion
+            print("[ESCUCHANDO] El servidor esta en la escucha")
+            
+            while True:
+                conn, addr = server.accept()  # Se establece la conexion con el cliente
+                print(f"[NUEVA CONEXION] {addr} conectado.")
+
+                filename = conn.recv(self.SIZE).decode(self.FORMAT)  # Se recibe el archivo (vacio)
+                print("[RECV] Archivo recivido.")
+                filename = filename.split(".")
+                filename[0] = filename[0]+"_recv."
+                filename = filename[0]+filename[1]
+                file_new = open(filename, 'w')  # Abrimos el archivo (vacio)
+                conn.send("Archivo recivido.".encode(self.FORMAT))
+
+                data = conn.recv(self.SIZE).decode(self.FORMAT)  # Se recibe la data (texto)
+                print(f"[RECV] Archivo de datos recibidos")
+                file_new.write(data)  # Se escribe la data en el el archivo
+                conn.send("Archivo de datos recivido".encode(self.FORMAT))  # Se almacena
+
+                file_new.close()  # Cerramos el archivo
+                conn.close()  # Cerramos la conexion con el cliente
+                print(f"[DESCONECTADO] {addr} desconectado")
+
         else: 
             self.protocol = socket.SOCK_DGRAM
-        print("[EMPEZANDO] El servidor esta iniciando")
-        server = socket.socket(socket.AF_INET, self.protocol)  # Se crea el servidor
-        server.bind(self.ADDR)  # Se establece la conexion
-        server.listen()  # Hacemos que el server este escuchando alguna peticion
-        print("[ESCUCHANDO] El servidor esta en la escucha")
 
-        while True:
-            conn, addr = server.accept()  # Se establece la conexion con el cliente
-            print(f"[NUEVA CONEXION] {addr} conectado.")
+            print("[EMPEZANDO] El servidor esta iniciando")
+            server = socket.socket(socket.AF_INET, self.protocol) # Se crea el servidor
+            server.bind(self.ADDR)  # Se establece la conexion
+    
+            print("[ESCUCHANDO] El servidor esta en la escucha")
+            f = open("recibido.jpg", "wb")
 
-            filename = conn.recv(self.SIZE).decode(self.FORMAT)  # Se recibe el archivo (vacio)
-            print("[RECV] Archivo recivido.")
-            filename = filename.split(".")
-            filename[0] = filename[0]+"_recv."
-            filename = filename[0]+filename[1]
-            file_new = open(filename, 'w')  # Abrimos el archivo (vacio)
-            conn.send("Archivo recivido.".encode(self.FORMAT))
-
-            data = conn.recv(self.SIZE).decode(self.FORMAT)  # Se recibe la data (texto)
-            print(f"[RECV] Archivo de datos recibidos")
-            file_new.write(data)  # Se escribe la data en el el archivo
-            conn.send("Archivo de datos recivido".encode(self.FORMAT))  # Se almacena
-
-            file_new.close()  # Cerramos el archivo
-            conn.close()  # Cerramos la conexion con el cliente
-            print(f"[DESCONECTADO] {addr} desconectado")
-
+            while True:
+                print('\nesperado recibir datos')
+                try:
+                    # Recibir datos del cliente.
+                    input_data, address = server.recv(1024).decode()
+                except socket.error:
+                    print("Error de lectura.")
+                    break
+                else:
+                    if input_data:
+                        if isinstance(input_data, bytes):
+                            end = input_data[0] == 1
+                        else:
+                            end = input_data == chr(1)
+                        if not end:
+                            # Almacenar datos.
+                            f.write(input_data)
+                        else:
+                            break
+            
+            print("El archivo se ha recibido correctamente.")
+                    
